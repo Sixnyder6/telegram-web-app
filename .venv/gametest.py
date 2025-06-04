bfc76462677adb9451658c6e4216e34322a2cf6a
Sixnyder6 <79064560720@mail.ru>
2025-06-04 21:23:52 +0300

    Обновил дизайн в стиле Galxe Web3

--- .venv/gametest.py
+++ .venv/gametest.py
@@ -1,14 +1,20 @@
 import telebot
+import logging
 
-TOKEN = "8001816336:AAGl5CAJRkbZ3vHBTUS-3P5GebH5S8bfRzM"
+TOKEN = "7875479927:AAHLi2odxR6TvWUk28fn0jhsoC1q-JYDDOM"
 bot = telebot.TeleBot(TOKEN)
 
-# Удаляем Webhook перед запуском polling
-bot.delete_webhook()
+# Настроим логи для диагностики
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+logging.info("Запуск бота...")
+
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
+    logging.info(f"Получена команда /start от {message.chat.id}")
+
     keyboard = telebot.types.InlineKeyboardMarkup()
     web_app_url = "https://sixnyder6.github.io/telegram-web-app/"
     web_app = telebot.types.WebAppInfo(web_app_url)
@@ -17,11 +23,17 @@ def start(message):
     keyboard.add(button)
 
     bot.send_message(message.chat.id, "Нажмите кнопку ниже, чтобы открыть Web App!", reply_markup=keyboard)
+    logging.info("Кнопка Web App отправлена")
+
 
 # Обработчик входящих данных из Web App
 @bot.message_handler(content_types=['web_app_data'])
 def handle_webapp_data(message):
     data = message.web_app_data.data  # Получаем JSON-данные
+    logging.info(f"Получены данные от Web App: {data}")
+
     bot.send_message(message.chat.id, f"🚀 Web App отправил: {data}")
+    logging.info("Ответ отправлен пользователю")
+
 
 bot.polling()
